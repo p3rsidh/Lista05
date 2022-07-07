0b8843622375ea604c79a4275a44a335b1fb8a15
p3rsidh <persidh.pimenta@gmail.com>
2022-07-07 18:42:18 -0300

    Criando funções para a pessoa, criando dialogo com o usuario

--- src/exercicio2/Main.java
+++ src/exercicio2/Main.java
@@ -1,7 +1,47 @@
 package exercicio2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Pessoa pessoinha = new Pessoa();
+        boolean continuarJogando = true;
+        System.out.println("Deseja criar uma nova pessoa? Digite 1 para sim e qualquer outro valor para não");
+        int escolha = scan.nextInt();
+        if (escolha == 1){
+            pessoinha.construirPessoa();
+            while (continuarJogando == true) {
+                System.out.println("Agora podemos brincar!!");
+                System.out.println("DIGITE 1 - para envelhecer " + pessoinha.nome);
+                System.out.println("DIGITE 2 - para engordar " + pessoinha.nome);
+                System.out.println("DIGITE 3 - para emagrecer " + pessoinha.nome);
+                System.out.println("DIGITE 4 - para fazer " + pessoinha.nome + " crescer");
+                System.out.println("DIGITE 5 - para mostrar as caracteristicas de " + pessoinha.nome);
+                System.out.println("DIGITE 6 - para sair");
+                int opcao = scan.nextInt();
+                switch (opcao){
+                    case 1:
+                        pessoinha.envelhecer();
+                        break;
+                    case 2:
+                        pessoinha.engordar();
+                        break;
+                    case 3:
+                        pessoinha.emagrecer();
+                        break;
+                    case 4:
+                        pessoinha.crescer();
+                        break;
+                    case 5:
+                        pessoinha.mostrarPessoa();
+                        break;
+                    case 6:
+                        continuarJogando = false;
+                        break;
+            }
+        }
+    }
 
     }
 }
--- src/exercicio2/Pessoa.java
+++ src/exercicio2/Pessoa.java
@@ -1,4 +1,92 @@
 package exercicio2;
 
+import java.util.Scanner;
+
 public class Pessoa {
-}
+    Scanner scan = new Scanner(System.in);
+    String nome;
+    float idade , peso , altura ;
+
+
+
+    public Pessoa() {
+        this.nome = "nome";
+        this.idade = 0;
+        this.peso = 0;
+        this.altura = 0;
+    }
+
+    public void construirPessoa(){
+        System.out.println("Vamos construir uma pessoa");
+        System.out.println("   (o.o)    " +
+                           "\n  /(  /)" +
+                           "\n   _| |_     ");
+        System.out.println("Digite o nome da pessoa:");
+        nome = scan.next();
+        System.out.println("Digite a idade desta pessoa");
+        idade = scan.nextFloat();
+        System.out.println("Digite o peso (se não quiser, coloque 0)");
+        peso = scan.nextFloat();
+        System.out.println("Digite a altura");
+        altura = scan.nextFloat();
+
+    }
+
+    public void mostrarPessoa(){
+        System.out.println(nome  +" tem "  +idade +" anos ");
+        System.out.println("pesa " +peso +" e mede "  +altura +"cm");
+    }
+    public void envelhecer(){
+        float maisIdade;
+        System.out.println("Deseja envelhecer quantos anos?");
+        maisIdade = scan.nextFloat();
+        System.out.println(maisIdade);
+        if(maisIdade > 0){
+            if(idade <= 20){
+                for(float i = idade; i < (maisIdade+idade); i++){
+                    altura += 0.5;
+                }
+            idade += maisIdade;
+            }else{
+                idade += maisIdade;
+            }
+        }else{
+            System.out.println("Valor invalido");
+        }
+        mostrarPessoa();
+    }
+
+    public void engordar(){
+    System.out.println("Digite quanto peso a pessoa vai ganhar");
+    double maisPeso = scan.nextDouble();
+    if (maisPeso > 0 ){
+        peso += maisPeso;
+    } else{
+        System.out.println("Valor invalido");
+    }
+    mostrarPessoa();
+    }
+
+    public void emagrecer(){
+        System.out.println("Digite quanto peso a pessoa vai perder");
+        double menosPeso = scan.nextDouble();
+        if (menosPeso > 0 ){
+            peso -= menosPeso;
+        } else{
+            System.out.println("Valor invalido");
+        }
+        mostrarPessoa();
+    }
+    public void crescer(){
+        System.out.println("Digite o quanto a pessoa vai crescer");
+        double crescendo = scan.nextDouble();
+        if (crescendo > 0){
+            altura += crescendo;
+        }else{
+            System.out.println("Valor invalido");
+        }
+        mostrarPessoa();
+        }
+    }
+
+
