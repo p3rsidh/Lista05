764888f3c26ba928b2c395d13ef368b69af3f67a
p3rsidh <persidh.pimenta@gmail.com>
2022-07-08 00:52:19 -0300

    Criando metodos na classe que permitem ao usuario manipular o valo da conta

--- src/exercicio3/ContaCorrente.java
+++ src/exercicio3/ContaCorrente.java
@@ -1,4 +1,37 @@
 package exercicio3;
 
+import java.util.Scanner;
+
 public class ContaCorrente {
+    Scanner scan = new Scanner(System.in);
+    String numeroConta, nomeCorrentista;
+    float saldo;
+    ContaCorrente(String numeroConta, String nomeCorrentista, float saldo){
+        this.numeroConta = numeroConta;
+        this.nomeCorrentista = nomeCorrentista;
+        this.saldo = 0;
+
+    }
+
+    public void depositoConta(){
+        System.out.println("Qual o valor que deseja depositar? ");
+        float somaSaldo = scan.nextFloat();
+        if (somaSaldo > 0){
+            saldo += somaSaldo;
+            System.out.println("O novo saldo e : " +saldo);
+        } else {
+            System.out.println("Valor invalido");
+        }
+    }
+
+    public void saqueConta(){
+        System.out.println("Qual valor deseja sacar? ");
+        float subtraiSaldo = scan.nextFloat();
+        if (subtraiSaldo > 0 && subtraiSaldo <= saldo){
+            saldo -= subtraiSaldo;
+            System.out.println("O novo saldo e de : " +saldo);
+        } else {
+            System.out.println("Valor invalido");
+        }
+    }
 }
