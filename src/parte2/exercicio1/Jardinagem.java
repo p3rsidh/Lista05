6d9c398d931a775bfdcb95335d7bf64b9ff3fe7d
p3rsidh <persidh.pimenta@gmail.com>
2022-07-08 18:41:03 -0300

    Criando metodos e entidades para modelar o jardim e os valores que se hasta. Criando classe main para dialogar com o usuario, receber informações e retornar valores.

--- src/parte2/exercicio1/Jardinagem.java
+++ src/parte2/exercicio1/Jardinagem.java
@@ -1,49 +1,63 @@
 package parte2.exercicio1;
 
+import java.util.Scanner;
+
 public class Jardinagem {
+    Scanner scan = new Scanner(System.in);
+    String nome;
+    float quantidadeMetros, quantidadeMetrosGrama, quilosAdubo, valorAdubo, valorMetroGrama,valorCorteGrama;
+    int quantidadePlantas;
+    float aduboUsando;
 
-
-    public class nomeJardim{
-        String nome;
-        nomeJardim(String nome){
+        public String nomeJardim(String nome){
             this.nome = nome;
-                    }
-    }
-
-    public class quantidadeMetros{
-        float quantidadeMetros;
-        quantidadeMetros(float quantidadeMetros){
-           this.quantidadeMetros = quantidadeMetros;
-
-            }
+        return nome;
         }
 
-        public class quantidadePlantas{
-        int quantidadePlantas;
-        quantidadePlantas(int quantidadePlantas){
+        public float quantidadeMetros(float quantidadeMetros){
+               this.quantidadeMetros = quantidadeMetros;
+        return quantidadeMetros;
+        }
+        public int quantidadePlantas(int quantidadePlantas){
             this.quantidadePlantas = quantidadePlantas;
+        return quantidadePlantas;
         }
-    }
-    public class quantidadeMetrosGrama{
-        float quantidadeMetrosGrama;
-        quantidadeMetrosGrama(float quantidadeMetrosGrama){
-            this.quantidadeMetrosGrama = quantidadeMetrosGrama;
-        }
-    }
-    public class quilosAdubo{
-        float quilosAdubo;
-        quilosAdubo(float quilosAdubo){
-            this.quilosAdubo = quilosAdubo;
-        }
-    }
-    public class valorAdubo{
-        float valorAdubo;
-        valorAdubo(float valorAdubo){
-            this.valorAdubo = valorAdubo;
-        }
-    }
-    public class valorMetroGrama{
 
-    }
-
-}
+        public float quantidadeMetrosGrama(float quantidadeMetrosGrama){
+                this.quantidadeMetrosGrama = quantidadeMetrosGrama;
+        return quantidadeMetrosGrama;
+        }
+        public float quilosAdubo(float quilosAdubo){
+                this.quilosAdubo = quilosAdubo;
+        return quilosAdubo;
+        }
+        public float valorAdubo(float valorAdubo){
+                this.valorAdubo = valorAdubo;
+        return valorAdubo;
+        }
+        public float valorMetroGrama(float valorMetroGrama){
+                this.valorMetroGrama = valorMetroGrama;
+        return valorMetroGrama;
+        }
+        public float valorCorteGrama(float valorCorteGrama){
+                this.valorCorteGrama = valorCorteGrama;
+        return valorCorteGrama;
+        }
+        //----------------------------------------------- METODOS -------------------------------------------
+            public float usarAdubo(){
+                System.out.println("Quantos metros de grama deseja adubar?");
+                float adubandoGrama = scan.nextFloat();
+                aduboUsando = ((adubandoGrama/2)*(100));
+                quilosAdubo = (aduboUsando / 1000);
+            System.out.println("Usaremos " +aduboUsando/1000 +"quilos de adubo");
+                return aduboUsando;
+        }
+            public float precoAdubo(){
+                    float precoAdubo = aduboUsando * valorAdubo;
+                    return precoAdubo;
+        }
+            public float precoCorteGrama(){
+                valorCorteGrama = quantidadeMetrosGrama * valorMetroGrama;
+                return valorCorteGrama;
+        }
+        }
--- src/parte2/exercicio1/Main.java
+++ src/parte2/exercicio1/Main.java
@@ -1,4 +1,29 @@
 package parte2.exercicio1;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Jardinagem jardim = new Jardinagem();
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Vamos cuidar do seu jardim!!");
+        System.out.println("Digite o nome dele:");
+        jardim.nomeJardim(scan.next());
+        System.out.println("Digite quantos metros tem seu jardim: ");
+        jardim.quantidadeMetros(scan.nextFloat());
+        System.out.println("Quantas plantas voce tem?");
+        jardim.quantidadePlantas(scan.nextInt());
+        System.out.println("Quantos metros de grama voce tem?");
+        jardim.quantidadeMetrosGrama(scan.nextFloat());
+        System.out.println("Quanto custa o quilo de adubo?");
+        jardim.valorAdubo(scan.nextFloat());
+        System.out.println("Quanto custa o metro da grama?");
+        jardim.valorMetroGrama(scan.nextFloat());
+        jardim.usarAdubo();
+        System.out.println("O preco do adubo e : " +jardim.precoAdubo());
+        System.out.println("O preco para cortar a grama e: " +jardim.precoCorteGrama());
+
+
+    }
 }
